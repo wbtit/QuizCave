7b7cba66151373834eee9fa901074f6df7c3b914
Gowtham-beep <gn86923@gmail.com>
2025-10-06 12:13:14 +0530

    feat: add update contest functionality and include related questions in contest details

--- src/controller/admin/contest.controller.js
+++ src/controller/admin/contest.controller.js
@@ -3,6 +3,7 @@ import { ApiResponse } from "../../util/ApiResponse.js";
 import { AsyncHandler } from "../../util/AsyncHandler.js";
 import { Contest } from "../../model/Contest.model.js";
 import path from "path";
+import { Question } from "../../model/Question.model.js";
 
 export const CreateContest = AsyncHandler(async (req, res) => {
     try {
@@ -76,7 +77,13 @@ export const GetContest = AsyncHandler(async (req, res) => {
             throw new ApiError(404, "Contest not found");
         }
 
-        return res.status(200).json(new ApiResponse(200, contest, "Contest details"));
+        // Fetch questions with the same set as contest
+        const questions = await Question.find({ set: contest.set });
+
+        return res.status(200).json(new ApiResponse(200, {
+            contest,
+            questions
+        }, "Contest details with questions"));
     } catch (error) {
         throw new ApiError(400, error.message);
     }
@@ -99,3 +106,43 @@ export const GetContests = AsyncHandler(async (req, res) => {
         throw new ApiError(400, error.message);
     }
 });
+
+export const UpdateContest = AsyncHandler(async (req, res) => {
+    try {
+        if (!req.user) {
+            throw new ApiError(401, "Unauthorized Access");
+        }
+
+        if (req.user.role !== "admin") {
+            throw new ApiError(401, "Unauthorized Access");
+        }
+
+        const {
+            name, duration, rules, startDate, endDate, active, registration, declared, set
+        } = req.body;
+
+        if (!name || !duration || !rules || !startDate || !endDate || !set) {
+            throw new ApiError(400, "All fields are required");
+        }
+
+        const updatedContest = await Contest.findByIdAndUpdate(req.params.id, {
+            name: name.trim(),
+            duration: Number(duration),
+            rules: rules,
+            startDate: new Date(startDate),
+            endDate: new Date(endDate),
+            active: active ? active : false,
+            registration: registration ? registration : true,
+            declared: declared ? declared : false,
+            set: set.trim().toUpperCase()
+        }, { new: true });
+
+        if (!updatedContest) {
+            throw new ApiError(404, "Contest not found");
+        }
+
+        return res.status(200).json(new ApiResponse(200, updatedContest, "Contest updated successfully"));
+    } catch (error) {
+        throw new ApiError(400, error.message);
+    }
+}); 
--- src/routes/admin/contest.routes.js
+++ src/routes/admin/contest.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { auth } from "../../middleware/auth.middleware.js";
 import {
-    CreateContest, GetContest, GetContests, RemoveContest,
+    CreateContest, GetContest, GetContests, RemoveContest,UpdateContest
 } from "../../controller/admin/contest.controller.js";
 
 export const AdminContestRouter = Router();
@@ -10,3 +10,4 @@ AdminContestRouter.get("/all", auth, GetContests);
 AdminContestRouter.get("/:id", auth, GetContest);
 AdminContestRouter.post("/create", auth, CreateContest);
 AdminContestRouter.delete("/remove/:id", auth, RemoveContest);
+AdminContestRouter.put("/update/:id", auth, UpdateContest);
